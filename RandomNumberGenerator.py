2a59bb7abbb4fb9b7684a32b42ce655c09f2fc5a
johnnnathan <johnnnathan@proton.me>
2024-07-03 14:07:40 +0300

    Finished video implementation

--- RandomNumberGenerator.py
+++ RandomNumberGenerator.py
@@ -2,39 +2,72 @@ import pathlib
 import random
 import cv2
 from pathlib import Path
+from PIL import Image
+
+RANDOM_FRAME = "random_frame.jpg"
+LIMIT = 1000
+
+def printVideoName(name: str):
+    print(f"Video selected: {name}")
+
+
+def getChosenColorIndex(listRGB: list) -> int:
+    totalValueOfRGB = listRGB[0] + listRGB[1] + listRGB[2]
+    index = totalValueOfRGB % 3
+    return index
 
 
 def getFile() -> Path:
     video_folder = Path('Videos')
     video_folder = Path(__file__).resolve().parent / video_folder
-    video_files = [file for file in video_folder.iterdir() if file.is_file() and file.suffix in ['.mp4', '.avi', '.mkv']]
+    video_files = [file for file in video_folder.iterdir() if
+                   file.is_file() and file.suffix in ['.mp4', '.avi', '.mkv']]
 
     if not video_files:
         print("Videos folder is empty")
     else:
         video = random.choice(video_files)
-        print(f"Video selected: {video}")
-        return video 
-
+        printVideoName(video)
+        return video
 
 
 def getFrame(file_path: Path):
     file_path_str = str(file_path)
-    print(file_path_str)
     vidcap = cv2.VideoCapture(file_path_str)
 
     totalFrames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    randomFrameNumber=random.randint(0, totalFrames)
+    randomFrameNumber = random.randint(0, totalFrames)
 
-    vidcap.set(cv2.CAP_PROP_POS_FRAMES,randomFrameNumber)
+    vidcap.set(cv2.CAP_PROP_POS_FRAMES, randomFrameNumber)
 
     success, image = vidcap.read()
 
     if success:
-        cv2.imwrite("random_frame.jpg", image)
+        cv2.imwrite(RANDOM_FRAME, image)
 
 
-file_path = getFile()
-getFrame(file_path)
-print("got here")
+def getTotalRGB() -> list[int]:
+    im = Image.open(RANDOM_FRAME)
+    listRGB = [0, 0, 0]
+    width, height = im.size
+    for x in range(width):
+        for y in range(height):
+            r, g, b = im.getpixel((x, y))
+            listRGB[0] += r
+            listRGB[1] += g
+            listRGB[2] += b
+
+    return listRGB
+
+
+def main():
+    file_path = getFile()
+    getFrame(file_path)
+    listRGB = getTotalRGB()
+    index = getChosenColorIndex(listRGB)
+    print(listRGB[index] % LIMIT)
+
+
+if __name__ == "__main__":
+    main()
